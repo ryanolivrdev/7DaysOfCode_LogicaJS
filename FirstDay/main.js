7e37ad55536b45005800c770d359b427f1bdd177
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-06-29 21:26:16 -0300

    First Day

---
+++ FirstDay/main.js
@@ -0,0 +1,51 @@
+
+// Sua tarefa de hoje é reescrever o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros:
+// let numeroUm = 1
+// let stringUm = '1'
+// let numeroTrinta = 30
+// let stringTrinta = '30'
+// let numeroDez = 10
+// let stringDez = '10'
+
+// if (COMPARAR O numeroUm e a stringUm) {
+//   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+// } else {
+//   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+// }
+
+// if (COMPARAR O numeroTrinta e a stringTrinta) {
+//   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+// } else {
+//   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+// }
+
+// if (COMPARAR O numeroDez e a stringDez) {
+//   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+// } else {
+//   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+// }
+
+let numberOne = 1
+let stringOne = '1'
+let numberThirty = 30
+let stringThirty = '30'
+let numberTen = 10
+let stringTen = '10'
+
+if (numberOne == stringOne) {
+  console.log('As variáveis numberOne e stringOne tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numberOne e stringOne não tem o mesmo valor')
+}
+
+if (numberThirty === stringThirty) {
+  console.log('As variáveis numberThirty e stringThirty tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numberThirty e stringThirty não tem o mesmo tipo')
+}
+
+if (numberTen == stringTen) {
+  console.log('As variáveis numberTen e stringTen tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numberTen e stringTen não tem o mesmo valor')
+}
